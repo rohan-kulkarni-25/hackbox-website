184f9da7bed44773a5c47b5989fef3c97f63edca
Rohan Kulkarni <rohank2502@gmail.com>
2022-02-17 09:09:33 +0530

    Added hero section -

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing: {
+        200: "50rem",
+        160: "40rem",
+        192: "48rem",
+        240: "60rem",
+        288: "72rem",
+      },
+    },
+  },
+  plugins: [],
+};
